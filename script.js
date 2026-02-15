a58a913824d94ef5ea808de508a4a92202b984cb
Rohan Rai <92577040+Rohanrai540@users.noreply.github.com>
2026-02-15 18:15:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 // your code here
+const nameInput = document.getElementById("name");
+const yearInput = document.getElementById("year");
+const button = document.getElementById("button");
+const urlText = document.getElementById("url");
+
+button.addEventListener("click", function() {
+
+  let baseURL = "https://localhost:8080/";
+  let params = [];
+
+  if(nameInput.value !== "") {
+    params.push(`name=${nameInput.value}`);
+  }
+
+  if(yearInput.value !== "") {
+    params.push(`year=${yearInput.value}`);
+  }
+
+  if(params.length > 0) {
+    baseURL += "?" + params.join("&");
+  }
+
+  urlText.innerText = baseURL;
+});
